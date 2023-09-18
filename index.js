ff19d65a2818c0040924894fc5bc468f47743e99
homurin <mochfajrin1211@gmail.com>
2023-09-18 22:01:08 +0700

    dinamis html page using nodejs

--- index.js
+++ index.js
@@ -11,66 +11,101 @@ const url = require("url");
 
 // non blocking
 
-const test = fs.readFile("./txt/start.txt", (err, parentData) => {
-  if (err) {
-    throw Error("Something went wrong");
-  }
-  console.info(parentData.toString("utf-8"));
-  fs.readFile(`./txt/${parentData}.txt`, "utf-8", (err, childData) => {
-    if (err) {
-      throw Error("Something went wrong");
-    }
-    console.info(childData);
-    fs.writeFile(`./txt/merger.txt`, `${parentData}\n${childData}`, (err) => {
-      if (err) {
-        throw Error("Something went wrong");
-      }
-      console.info(`Sukses`);
-      const readThis = fs.readFileSync("./txt/read-this.txt", "utf-8");
-      const finalTxt = fs.readFileSync("./txt/final.txt", "utf-8");
+// const test = fs.readFile("./txt/start.txt", (err, parentData) => {
+//   if (err) {
+//     throw Error("Something went wrong");
+//   }
+//   console.info(parentData.toString("utf-8"));
+//   fs.readFile(`./txt/${parentData}.txt`, "utf-8", (err, childData) => {
+//     if (err) {
+//       throw Error("Something went wrong");
+//     }
+//     console.info(childData);
+//     fs.writeFile(`./txt/merger.txt`, `${parentData}\n${childData}`, (err) => {
+//       if (err) {
+//         throw Error("Something went wrong");
+//       }
+//       console.info(`Sukses`);
+//       const readThis = fs.readFileSync("./txt/read-this.txt", "utf-8");
+//       const finalTxt = fs.readFileSync("./txt/final.txt", "utf-8");
 
-      fs.writeFile("./txt/challenge.txt", `${readThis}\n${finalTxt}`, (err) => {
-        if (err) {
-          throw new Error("Something went wrong");
-        }
-        console.info(`success merging read-this and final`);
-      });
-    });
-  });
-});
-console.info(test);
-console.info("Hi F2W nunggu read file yah");
+//       fs.writeFile("./txt/challenge.txt", `${readThis}\n${finalTxt}`, (err) => {
+//         if (err) {
+//           throw new Error("Something went wrong");
+//         }
+//         console.info(`success merging read-this and final`);
+//       });
+//     });
+//   });
+// });
+// console.info(test);
+// console.info("Hi F2W nunggu read file yah");
+function replaceTemplate(template, product) {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+}
+const buffer = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(buffer);
+const overviewPage = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  "utf-8"
+);
+const productTemplate = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  "utf-8"
+);
+const productCardTemplate = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  console.info(req.url);
+  const { query, pathname: pathName } = url.parse(req.url, true);
+
+  // hello page
   if (pathName === "/hello") {
     res.end("Hello FS2 ／人◕ ‿‿ ◕人＼");
-  } else if (pathName === "/product") {
-    res.end(
-      JSON.stringify({
-        data: "Ini produk ／人◕ ‿‿ ◕人＼",
-      })
-    );
+    //product page
+
+    // overview page
   } else if (pathName === "/overview") {
-    const overviewPage = fs.readFileSync(
-      `${__dirname}/templates/overview.html`
-    );
     res.writeHead(200, {
       "Content-Type": "text/html",
     });
-    res.end(overviewPage);
+    const productCardHtml = dataObj.map((e) => {
+      return replaceTemplate(productCardTemplate, e);
+    });
+    const output = overviewPage.replace("{%PRODUCT_CARDS%}", productCardHtml);
+    res.end(output);
+    //overview api
   } else if (pathName === "/api") {
-    const buffer = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
     res.writeHead(200, {
       "Content-Type": "application/json",
     });
     res.end(buffer);
+    //product page
+  } else if (pathName === "/product") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(productTemplate, product);
+    res.end(output);
   } else {
     res.writeHead(404, {
       "Content-Type": "text/html",
     });
-    res.end(`<h1>Gak ada apa-apa ／人◕ ‿‿ ◕人＼</h1>`);
+    res.end(`<h1>Gak ada apa-apa</h1>`);
   }
 });
 
